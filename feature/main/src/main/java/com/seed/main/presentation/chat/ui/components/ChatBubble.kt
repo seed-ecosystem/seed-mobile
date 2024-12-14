f9a5be086af13b17ab9d319ba617a4815a7b0035
demn <demndevel@gmail.com>
2024-12-15 00:39:34 +0300

    implement text avatar

--- feature/main/src/main/java/com/seed/main/presentation/chat/ui/components/ChatBubble.kt
+++ feature/main/src/main/java/com/seed/main/presentation/chat/ui/components/ChatBubble.kt
@@ -1,31 +1,24 @@
 package com.seed.main.presentation.chat.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
@@ -61,10 +54,8 @@ fun ChatBubble(
 					modifier = Modifier
 						.fillMaxHeight()
 				) {
-					Box(
-						modifier = Modifier
-							.size(48.dp)
-							.background(Color.LightGray, CircleShape)
+					TextAvatar(
+						text = message.authorName,
 					)
 				}
 
