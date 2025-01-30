c5b800d067546d38d6e3fc4327d9c798038b77af
demn <demndevel@gmail.com>
2025-01-30 13:37:27 +0500

    feat(app): implement logging for crashlytics reports

--- app/src/main/java/com/seed/mobile/Logger.kt
+++ app/src/main/java/com/seed/mobile/Logger.kt
@@ -1,14 +1,18 @@
 package com.seed.mobile
 
 import android.util.Log
+import com.google.firebase.Firebase
+import com.google.firebase.crashlytics.crashlytics
 import com.seed.domain.Logger
 
 class LoggerImpl : Logger {
 	override fun d(tag: String, message: String) {
 		Log.d(tag, message)
+		Firebase.crashlytics.log("D\t$tag\t$message")
 	}
 
 	override fun e(tag: String, message: String) {
 		Log.e(tag, message)
+		Firebase.crashlytics.log("E\t$tag\t$message")
 	}
 }
